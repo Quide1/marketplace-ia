348fc09211967fef99ae72a288948dee30cf7173
Quide1 <ezequielr.dev@gmail.com>
2024-09-20 20:37:20 -0300

    Create data table

--- src/components/TableData.tsx
+++ src/components/TableData.tsx
@@ -1,21 +1,46 @@
-import React from 'react'
-import { PublicationData } from '../types/publicationData'
+import { PublicationData } from "../types/publicationData";
 
-function TableData(data:PublicationData) {
+const cleanDescription = (desc: string) => {
+  return desc.replace(/Ver menos/g, "");
+};
+
+function TableData({
+  image,
+  price,
+  link,
+  title,
+  description,
+}: PublicationData) {
   return (
-    <>
-    <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">${data.title}</td>
-    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">${data.price}</td>
-    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">${data.description}</td>
-    <td className="px-6 py-4 whitespace-nowrap text-sm text-blue-500">
-      <a href="${data.link}" target="_blank" className="hover:underline">Ver enlace</a>
-    </td>
-    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-      <img src="${data.image}" alt="Car Image" className="w-32 h-32 object-cover rounded"/>
-    </td>
-    </>
-
-  )
+    <tr className="">
+      <td className="text-sm text-gray-500 w-[150px]">
+        <img
+          src={image}
+          alt="Car Image"
+          className="w-32 h-32 object-cover rounded"
+        />
+      </td>
+      <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+        <div className="flex flex-col gap-2">
+          <p>{price}</p>
+          <a
+            href={link}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-blue-700 font-bold"
+          >
+            Enlace
+          </a>
+        </div>
+      </td>
+      <td className="px-6 py-4  text-sm font-medium text-gray-900 max-w-[250px]">
+        {title}
+      </td>
+      <td className="px-6 py-4  text-sm text-gray-500">
+        {cleanDescription(description)}
+      </td>
+    </tr>
+  );
 }
 
-export default TableData
+export default TableData;
