b53736108fba93e91689bf4b1bfa778ce7bcbccb
Quide1 <ezequielr.dev@gmail.com>
2024-09-21 20:25:27 -0300

    change line

--- src/components/TableData.tsx
+++ src/components/TableData.tsx
@@ -1,16 +1,24 @@
+import { useDataContext } from "../context/dataContext";
 import { PublicationData } from "../types/publicationData";
 
 const cleanDescription = (desc: string) => {
   return desc.replace(/Ver menos/g, "");
 };
 
+import { StarIcon, CircleXIcon } from "lucide-react";
 function TableData({
   image,
   price,
   link,
   title,
   description,
+  uuid,
 }: PublicationData) {
+  const dataContext = useDataContext();
+
+  if (!dataContext) return null; // Manejar si no hay contexto (en casos donde el componente no esté envuelto en el provider)
+
+  const { deleteItem, addOfFavData } = dataContext;
   return (
     <tr className="">
       <td className="text-sm text-gray-500 w-[150px]">
@@ -38,6 +46,25 @@ function TableData({
       </td>
       <td className="px-6 py-4  text-sm text-gray-500">
         {cleanDescription(description)}
+      </td>
+      <td>
+        <div className="flex flex-col gap-2 p-2">
+          <button
+            className="hover:text-red-700 hover:scale-110 cursor-pointer transition-all"
+            onClick={() => deleteItem(uuid)}
+          >
+            <CircleXIcon />
+          </button>
+          <button
+            className="hover:text-yellow-600 hover:scale-110 cursor-pointer transition-all"
+            onClick={() => {
+              addOfFavData({ image, price, link, title, description, uuid });
+              deleteItem(uuid)
+            }}
+          >
+            <StarIcon />
+          </button>
+        </div>
       </td>
     </tr>
   );
