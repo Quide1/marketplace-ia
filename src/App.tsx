36b5e55ca9a5dcfe4e70c54ddd14287f05add049
Quide1 <ezequielr.dev@gmail.com>
2024-09-26 18:39:43 -0300

    Add ia Question

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@
 import FavoritesSection from "./sections/FavoritesSection";
 import ItemsSection from "./sections/ItemsSection";
 import Banner from "./sections/Banner";
+import IaQuestion from "./sections/IaQuestion";
 function App() {
  
   return (
     <main className="bg-blue-950 min-h-screen flex-col flex items-center bg-[conic-gradient(at_right,_var(--tw-gradient-stops))] from-gray-400 via-gray-600 to-blue-800">
       <Banner/>
       <FavoritesSection/>
+      <IaQuestion/>
       <ItemsSection/>
     </main>
   );
